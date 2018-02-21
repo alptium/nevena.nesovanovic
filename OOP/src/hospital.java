121c0588dde3598c9f13597d9297f198d5c3b4e5
nevenan <nevenanesovanovic@gmail.com>
2018-02-21 20:07:19 +0100

    Created the Calculator project

---
+++ OOP/src/hospital.java
@@ -0,0 +1,9 @@
+
+public class hospital {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- hello-world/src/helloworld/Main.java
+++ hello-world/src/helloworld/Main.java
@@ -3,11 +3,6 @@ package helloworld;
 public class Main {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-
-=======
-		
->>>>>>> 25037d23b400a7b16950606fa7366a241ed82e5b
 
 		System.out.println("Hello World!");
 		System.out.println("My name is Nevena!");
